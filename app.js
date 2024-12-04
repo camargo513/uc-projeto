1b03f48b46ceac4c589a72f69b3dcca097ed34e0
felipe <escolafelipe50@gmail.com>
2024-12-03 21:01:04 -0300

    subindo novo felipe

--- app.js
+++ app.js
@@ -9,13 +9,48 @@ app.get('/', (requisicao, resposta) => {
     resposta.status(200).send(carros2024);
 });
 
+app.get('/:sigla', (requisicao, resposta) => {
+    const siglaInformada = requisicao.params.sigla.toUpperCase(); // Obtendo a Sigla
+    const carro = carros2024.find((infoCarro) => infoCarro.sigla === siglaInformada); // Busca o carro pela sigla
+    if (!carro) {
+        // ! Vazio Not
+        // Se o carro não for enctrado, retorna erro 404
+        resposta
+        .status(404)
+        .send(
+            'Não existe um carro com a sigla informada!' //Mensagem de erro
+        );
+    return;
+    }
+    resposta.status(200).send(carro); // Se encontrado a sigla retorna a resposta correta
+});
+
+app.post('/', (req, res) => {
+    const novoCarro = req.body; // Obtém o corpo enviado para incluir um carro
+    carros2024.push(novoCarro); // Adiciona o novo carro à lista de carros.
+    res.status(200).send(novoCarro); // Retorna o carro adicionado com status 200 (Ok).
+});
 
 
-
-
-
-
-//Inicia o servidor da porta 3000
+app.put('/:sigla', (req, res) =>{
+    const siglaInformada = req.params.sigla.toUpperCase();
+    const carroSelecionado = carros2024.find(c => c.sigla === siglaInformada);
+    if(!carroSelecionado) {
+        res
+        .status(404)
+        .send(
+            'Não existem um carro com a sigla informada!'
+        );
+        return
+    };
+    const campos = Object.keys(req.body);
+    for (let campo of campos) {
+        carroSelecionado[campo] = req.body[campo];
+    }
+    res.status(200).send(carroSelecionado);
+});
+// Inicia o servidor na porta 3000:
 app.listen(3000,() => console.log("Servidor Rodando com Sucesso"));
 
-// para testar, abrir o servidor e digitar 'localhost:3000/'
+// node app.js
+// localhost:3000/
